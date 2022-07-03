e584479fc7e4a1bd92fecb13184694d3e4b15252
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2022-07-04 09:20:52 +1000

    updated JS file

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,32 @@
+function getWeather() {
+    const inputData=document.getElementById("inputData");
+    const displayName=document.getElementById("displayName");
+    displayName.innerHTML="--"+inputData.value+"--"
 
-var searchButton=$('#searchBtn');
 
+//getWeather();
 
+fetch("https://api.openweathermap.org/data/2.5/forecast?q="+inputData.value+"&appid=ba55bf9d483b2020057a460163c2f51d")
+.then(response => response.json())
+.then(data =>{
+   for(i=0; i<5;i++){
+        document.getElementById("day" +(i+1)+"date").innerHTML="date:" + Number(data.list[i].dt_txt);
+    }
+    
+    for(i=0; i<5;i++){
+        document.getElementById("day" +(i+1)+"temp").innerHTML="temp:" + Number(data.list[i].main.temp -300.78).toFixed(1);
+    }
+    for(i=0; i<5;i++){
+       document.getElementById("day" +(i+1)+"wind").innerHTML="wind:" + Number(data.list[i].wind.speed).toFixed(1);
+    }
 
+    for(i=0; i<5;i++){
+        document.getElementById("day" +(i+1)+"humidity").innerHTML="humidity:" + Number(data.list[i].main.humidity -80);
+    }
 
+    
 
-var cityInput;//city input
-var inputCity=[];// save city input
-
-searchButton.click(function ()
-{
-cityInput=$('#inputData').val();
-save();
-display();
 })
+
+}
 
