afcf97584318ada09473c31dca1a03dd9db58b75
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2022-07-05 17:37:41 +1000

    updated Javascrpt

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,40 @@
 function getWeather() {
     const inputData=document.getElementById("inputData");
     const displayName=document.getElementById("displayName");
-    displayName.innerHTML="--"+inputData.value+"--"
+    displayName.innerHTML=" "+inputData.value+" "
+    const cityName=document.getElementById("cityName");
 
 
 //getWeather();
 
 fetch("https://api.openweathermap.org/data/2.5/forecast?q="+inputData.value+"&appid=ba55bf9d483b2020057a460163c2f51d")
 .then(response => response.json())
-.then(data =>{
-   for(i=0; i<5;i++){
-        document.getElementById("day" +(i+1)+"date").innerHTML="date:" + Number(data.list[i].dt_txt);
-    }
-    
-    for(i=0; i<5;i++){
-        document.getElementById("day" +(i+1)+"temp").innerHTML="temp:" + Number(data.list[i].main.temp -300.78).toFixed(1);
-    }
-    for(i=0; i<5;i++){
-       document.getElementById("day" +(i+1)+"wind").innerHTML="wind:" + Number(data.list[i].wind.speed).toFixed(1);
-    }
+.then(data => {
+    console.log(data)
+    return data;
+ } )
 
-    for(i=0; i<5;i++){
-        document.getElementById("day" +(i+1)+"humidity").innerHTML="humidity:" + Number(data.list[i].main.humidity -80);
-    }
+.then(data =>{  
 
     
 
+    for(i=0; i<5;i++){
+        //Default temperature in data is Kelvin and converting it into celcius by substracting -273.15
+        document.getElementById("temp" +(i+1)).innerHTML="temp:" +Number(data.list[i].main.temp -273.15).toFixed(1);
+        document.getElementById("humidity" +(i+1)).innerHTML="Humidity:" +Number(data.list[i].main.humidity);
+        document.getElementById("wind" +(i+1)).innerHTML="Wind:" +Number(data.list[i].wind.speed);
+        document.getElementById("date" +(i+1)).innerHTML="Date:" +Number(data.list[i].dt_txt);
+    }
+        document.getElementById("dayTemp").innerHTML="temp:" +Number(data.list[0].main.temp -273.15).toFixed(1);
+        document.getElementById("dayHumidity").innerHTML="Humidity:" +Number(data.list[0].main.humidity);
+        document.getElementById("dayWind").innerHTML="Wind:" +Number(data.list[0].wind.speed);
+        
+       // cityName.text(data.name + " " +moment().format('MM/DD/YYYY'));
+        document.getElementById("cityName").innerHTML="City:" +(data.city[0].name);
+        
+
+    
+    
 })
-
 }
-
+    
