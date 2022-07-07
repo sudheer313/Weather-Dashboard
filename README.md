716901ff913d879915550c0bf7a5d365ffd4a058
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2022-07-07 10:14:36 +1000

    updated JS

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Weather-Dashboard
+
+https://sudheer313.github.io/Weather-Dashboard/
 ba55bf9d483b2020057a460163c2f51d
 https://api.openweathermap.org/data/2.5/forecast?q=hyderabad,%20India&appid=ba55bf9d483b2020057a460163c2f51d
 
@@ -17,3 +19,7 @@ $.ajax({
     $( "#weather-temp" ).html( "<strong>" + result + "</strong> degrees" );
   }
 });
+
+
+https://api.openweathermap.org/data/2.5/onecall?lat=37.81&lon=-144.96&exclude=hourly,alerts&units=imperial&appid=ba55bf9d483b2020057a460163c2f51d
+## melbourne 
--- assets/script.js
+++ assets/script.js
@@ -1,73 +1,70 @@
+var cities = JSON.parse(localStorage.getItem("cities") || "[]");
+var inputData = document.getElementById("inputData");
+var displayName = document.getElementById("displayName");
+var serachCity = "";
+document.getElementById("searchBtn").addEventListener("click", getWeather);
 
-const cities=JSON.parse(localStorage.getItem("cities")||"[]");
 function getWeather() {
-    const inputData=document.getElementById("inputData");
-    const displayName=document.getElementById("displayName");
-    displayName.innerHTML=" "+inputData.value+" "
-    const cityName=document.getElementById("searchCity");
+  displayName.innerHTML = inputData.value;
+  const cityName = document.getElementById("searchCity");
+  cities.push(inputData.value);
+  localStorage.setItem("cities", JSON.stringify(cities, null, 2));
+  for (i = 0; i <= cities.length; i++) localStorage.getItem("cities");
+  console.log(cities);
 
+  serachCity = inputData.value;
+  fetchApi("");
+}
 
-
-//getWeather();
-//const inpValue=document.getElementById("inputData");
-//var IsOutput= localStorage.setItem("IsOutput",inputData );
-// console.log("IsOutput");
-
-
-function fetchApi()
-{
-cities.push(inputData.value);
-localStorage.setItem("cities",JSON.stringify(cities,null,2))
-for(i=0;i<=cities.length;i++)
-localStorage.getItem("cities");
- //cityName=(cities[i].value);
-//append.display[i];
-
-//
-//window.onload = function () {
-       //code that runs on load
-  // };
-
-
-console.log(cities);
-fetch("https://api.openweathermap.org/data/2.5/forecast?q="+inputData.value+"&appid=ba55bf9d483b2020057a460163c2f51d")
-.then(response => response.json())
-.then(data => {
-    console.log(data)
-    return data;
- } )
-
-.then(data =>{  
-
+function fetchApi() {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      inputData.value +
+      "&appid=ba55bf9d483b2020057a460163c2f51d"
+  )
+    .then(function (response) {
+      // After getting into the API this function will now happen (promise)
+      return response.json(); // Returns the information as JSON
+    })
+    .then(function (
+      data // We take that data and use it inside this function to start outputting our information
+    ) {
+      console.log(data);
+      return data;
+      
+    })
     
-
-    for(i=0; i<5;i++){
+    .then((data) => {
+      for (i = 0; i < 5; i++) {
         //Default temperature in data is Kelvin and converting it into celcius by substracting -273.15
-        document.getElementById("temp" +(i+1)).innerHTML="temp:" +Number(data.list[i].main.temp -273.15).toFixed(1);
-        document.getElementById("humidity" +(i+1)).innerHTML="Humidity:" +Number(data.list[i].main.humidity);
-        document.getElementById("date" +(i+1)).innerHTML="Date:" + data.list[i].dt_txt;
-        document.getElementById("wind" +(i+1)).innerHTML="Wind:" +Number(data.list[i].wind.speed);
+        document.getElementById("temp" + (i + 1)).innerHTML =
+          "temp:" + Number(data.list[i].main.temp - 273.15).toFixed(1);
+        document.getElementById("humidity" + (i + 1)).innerHTML =
+          "Humidity:" + Number(data.list[i].main.humidity);
+        document.getElementById("date" + (i + 1)).innerHTML =
+          "Date:" + data.list[i].dt_txt;
+        document.getElementById("wind" + (i + 1)).innerHTML =
+          "Wind:" + Number(data.list[i].wind.speed);
+         
         //document.getElementById("date" +(i+1)).innerHTML="Date:" +Number(data.list[i].dt_txt);
-    }
-        document.getElementById("dayTemp").innerHTML="temp:" +Number(data.list[0].main.temp -273.15).toFixed(1);
-        document.getElementById("dayHumidity").innerHTML="Humidity:" +Number(data.list[0].main.humidity);
-        document.getElementById("dayWind").innerHTML="Wind:" +Number(data.list[0].wind.speed);
-        
-       // cityName.text(data.name + " " +moment().format('MM/DD/YYYY'));
-        document.getElementById("cityName").innerHTML="City:" +(data.city.name);
-        
-     
-})
+      }
+      
+      document.getElementById("dayTemp").innerHTML =
+        "temp:" + Number(data.list[0].main.temp - 273.15).toFixed(1);
+      document.getElementById("dayHumidity").innerHTML =
+        "Humidity:" + Number(data.list[0].main.humidity);
+      document.getElementById("dayWind").innerHTML =
+        "Wind:" + Number(data.list[0].wind.speed);
 
+      document.getElementById("cityName").innerHTML = "City:" + data.city.name;
+    });
 }
 
-fetchApi();
-}
 window.onload = function () {
-    let html="";
-    for(let i = 0; i < cities.length; i++){
-        console.log(cities[i]);
-        html += '<li><button>' + cities[i] + '</button></li>'
-        }
-        document.querySelector('#seachHistory').innerHTML = html;
-  };
+  let html = "";
+  for (let i = 0; i < cities.length; i++) {
+    console.log(cities[i]);
+    html += "<li><button>" + cities[i] + "</button></li>";
+  }
+  document.querySelector("#seachHistory").innerHTML = html;
+};
