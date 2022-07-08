2b1476b4921222b7a145432a1734a883247fa5e1
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2022-07-08 21:31:07 +1000

    updated JS

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Weather-Dashboard
 
 https://sudheer313.github.io/Weather-Dashboard/
+
+sudheer313
+/
+Weather-Dashboard
 ba55bf9d483b2020057a460163c2f51d
 https://api.openweathermap.org/data/2.5/forecast?q=hyderabad,%20India&appid=ba55bf9d483b2020057a460163c2f51d
 
@@ -23,3 +27,18 @@ $.ajax({
 
 https://api.openweathermap.org/data/2.5/onecall?lat=37.81&lon=-144.96&exclude=hourly,alerts&units=imperial&appid=ba55bf9d483b2020057a460163c2f51d
 ## melbourne 
+yes the icon is in the data under the weather element
+
+"wind_gust": 16.17,
+"weather": [
+{
+"id": 804,
+"main": "Clouds",
+"description": "overcast clouds",
+"icon": "04d"
+}
+],
+ http://openweathermap.org/img/wn/04d@2x.png
+ you have to substitute your icon code in the URL above with whatever code is return for the day you are interested in
+10:26
+they doc link I provided lists all of the icons icon codes ending d are for day and ending in n are for night
--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
 var cities = JSON.parse(localStorage.getItem("cities") || "[]");
 var inputData = document.getElementById("inputData");
+var dayUV=document.getElementById("dayUV");
 var displayName = document.getElementById("displayName");
 var serachCity = "";
 document.getElementById("searchBtn").addEventListener("click", getWeather);
-
 function getWeather() {
   displayName.innerHTML = inputData.value;
   const cityName = document.getElementById("searchCity");
@@ -31,30 +31,98 @@ function fetchApi() {
     ) {
       console.log(data);
       return data;
-      
     })
-    
+
     .then((data) => {
+    
+      fetch(
+        "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+          data.city.coord.lat +
+          "&lon=" +
+          data.city.coord.lon +
+          "&exclude=hourly,alerts&units=imperial&appid=ba55bf9d483b2020057a460163c2f51d"
+      ) // Calls our  second API for the information we need.
+        .then(function (
+          response // After gettubg ubti the API, this function will now happen (promise)
+        ) {
+          return response.json(); // Returns the data we need as a JSON
+        })
+        .then(function (
+          data2 // We take that data and use it inside this function to start outputting our information.
+        ) {
+          console.log(data2);
+          document.getElementById("dayUV").innerHTML =
+         Number(data2.current.uvi);
+          // Console log the data so you can see everything the API provides
+          //dayUV.text(data2.current.uvi);
+         }); // Outputs the current days UV Index
+
+
+
+      //displaying temperature from data
+      //converting from Kelvin to celcius
+      document.getElementById("temp" + 1).innerHTML =
+        "temp:" + Number(data.list[0].main.temp - 273.15).toFixed(1)+"°C";
+      document.getElementById("temp" + 2).innerHTML =
+        "temp:" + Number(data.list[8].main.temp - 273.15).toFixed(1)+"°C";
+      document.getElementById("temp" + 3).innerHTML =
+        "temp:" + Number(data.list[16].main.temp - 273.15).toFixed(1)+"°C";
+      document.getElementById("temp" + 4).innerHTML =
+        "temp:" + Number(data.list[24].main.temp - 273.15).toFixed(1)+"°C";
+      document.getElementById("temp" + 5).innerHTML =
+        "temp:" + Number(data.list[32].main.temp - 273.15).toFixed(1)+"°C";
+
+      //displaying humidity
+      document.getElementById("humidity" + 1).innerHTML =
+        "Humidity:" + Number(data.list[0].main.humidity)+ "%";
+      document.getElementById("humidity" + 2).innerHTML =
+        "Humidity:" + Number(data.list[8].main.humidity)+ "%";
+      document.getElementById("humidity" + 3).innerHTML =
+        "Humidity:" + Number(data.list[16].main.humidity)+ "%";
+      document.getElementById("humidity" + 4).innerHTML =
+        "Humidity:" + Number(data.list[24].main.humidity)+ "%";
+      document.getElementById("humidity" + 5).innerHTML =
+        "Humidity:" + Number(data.list[32].main.humidity)+ "%";
+
+      //displaying wind from available data
+      document.getElementById("wind" + 1).innerHTML =
+        "Wind:" + Number(data.list[0].wind.speed)+"MPH";
+      document.getElementById("wind" + 2).innerHTML =
+        "Wind:" + Number(data.list[1].wind.speed)+"MPH";
+      document.getElementById("wind" + 3).innerHTML =
+        "Wind:" + Number(data.list[2].wind.speed)+"MPH";
+      document.getElementById("wind" + 4).innerHTML =
+        "Wind:" + Number(data.list[3].wind.speed)+"MPH";
+      document.getElementById("wind" + 5).innerHTML =
+        "Wind:" + Number(data.list[4].wind.speed)+"MPH";
+
+      // displaying date from available data
+      document.getElementById("date" + 1).innerHTML =
+        "Date:" + data.list[0].dt_txt;
+      document.getElementById("date" + 2).innerHTML =
+        "Date:" + data.list[8].dt_txt;
+      document.getElementById("date" + 3).innerHTML =
+        "Date:" + data.list[16].dt_txt;
+      document.getElementById("date" + 4).innerHTML =
+        "Date:" + data.list[24].dt_txt;
+      document.getElementById("date" + 5).innerHTML =
+        "Date:" + data.list[32].dt_txt;
+
       for (i = 0; i < 5; i++) {
-        //Default temperature in data is Kelvin and converting it into celcius by substracting -273.15
-        document.getElementById("temp" + (i + 1)).innerHTML =
-          "temp:" + Number(data.list[i].main.temp - 273.15).toFixed(1);
-        document.getElementById("humidity" + (i + 1)).innerHTML =
-          "Humidity:" + Number(data.list[i].main.humidity);
-        document.getElementById("date" + (i + 1)).innerHTML =
-          "Date:" + data.list[i].dt_txt;
-        document.getElementById("wind" + (i + 1)).innerHTML =
-          "Wind:" + Number(data.list[i].wind.speed);
-         
-        //document.getElementById("date" +(i+1)).innerHTML="Date:" +Number(data.list[i].dt_txt);
+        document.getElementById("img" + (i + 1)).src =
+          " http://openweathermap.org/img/wn/" +
+          data.list[i].weather[0].icon +
+          ".png";
       }
-      
+
+      //main card
       document.getElementById("dayTemp").innerHTML =
-        "temp:" + Number(data.list[0].main.temp - 273.15).toFixed(1);
+        "temp:" + Number(data.list[0].main.temp - 273.15).toFixed(1)+"°C";
       document.getElementById("dayHumidity").innerHTML =
-        "Humidity:" + Number(data.list[0].main.humidity);
+        "Humidity:" + Number(data.list[0].main.humidity)+ "%";
       document.getElementById("dayWind").innerHTML =
-        "Wind:" + Number(data.list[0].wind.speed);
+        "Wind:" + Number(data.list[0].wind.speed)+"MPH";
+        
 
       document.getElementById("cityName").innerHTML = "City:" + data.city.name;
     });
@@ -66,5 +134,8 @@ window.onload = function () {
     console.log(cities[i]);
     html += "<li><button>" + cities[i] + "</button></li>";
   }
+  // need to add id to this button
   document.querySelector("#seachHistory").innerHTML = html;
+  // html.setAttribute('id', 'historyButton');
+  // console.log(document.getElementById('historyButton').id)
 };
