300c163576c0c6dd1e14dfd09a48311953ff443e
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2022-07-20 20:25:54 +1000

    updated JS

--- assets/script.js
+++ assets/script.js
@@ -132,11 +132,13 @@ window.onload = function () {
   let html = "";
   for (let i = 0; i < cities.length; i++) {
     console.log(cities[i]);
-    html += "<li><button class='searchButton'>" + cities[i] + "</button></li>";
+    html += "<li><button id='searchButton'>" + cities[i] + "</button></li>";
   }
   // need to add id to this button
   document.querySelector("#seachHistory").innerHTML = html;
-  document.querySelector(".searchButton").addEventListener("click", ()=>{console.log(this)})
+  document.querySelector("#searchButton").innerHTML= searchButton;
+  searchButton.addEventListener("click",fetchApi());
+  //.addEventListener("click", ()=>{console.log(this)})
   console.log(html);
   // html.setAttribute('id', 'historyButton');
 
